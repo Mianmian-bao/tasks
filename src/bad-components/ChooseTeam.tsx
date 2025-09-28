fc0397ad41050cd1d86d496c1ff739b1cb98293e
Mianmian Bao <bao@udel.edu>
2025-09-27 22:58:51 -0400

    components

--- src/bad-components/ChooseTeam.tsx
+++ src/bad-components/ChooseTeam.tsx
@@ -11,10 +11,13 @@ const PEOPLE = [
 ];
 
 export function ChooseTeam(): React.JSX.Element {
-    const [allOptions, setAllOptions] = useState<string[]>(PEOPLE);
+    const [allOptions] = useState<string[]>(PEOPLE);
     const [team, setTeam] = useState<string[]>([]);
 
-    function chooseMember() {
+    function chooseMember(newMember: string) {
+        setTeam((team) =>
+            team.includes(newMember) ? team : [...team, newMember],
+        );
         /*
         if (!team.includes(newMember)) {
             team.push(newMember);
@@ -23,6 +26,7 @@ export function ChooseTeam(): React.JSX.Element {
     }
 
     function clearTeam() {
+        setTeam([]);
         /*
         team = [];
         */
@@ -36,7 +40,12 @@ export function ChooseTeam(): React.JSX.Element {
                     {allOptions.map((option: string) => (
                         <div key={option} style={{ marginBottom: "4px" }}>
                             Add{" "}
-                            <Button onClick={chooseMember} size="sm">
+                            <Button
+                                onClick={() => {
+                                    chooseMember(option);
+                                }}
+                                size="sm"
+                            >
                                 {option}
                             </Button>
                         </div>
